a29cbd3bd8fccc1bef28032f1f2b06951c6e7445
mcapizzi <mcapizzi@email.arizona>
2015-01-24 08:33:13 -0700

    began Main

---
+++ src/main/scala/homework01/Main.scala
@@ -0,0 +1,16 @@
+package homework01
+
+/**
+ * Created by mcapizzi on 1/24/15.
+ */
+
+import CalculateMED._
+import MinimumEditDistance._
+
+//args(0): String = [target name]
+//args(1): List[String] = List([misspellings in list format])
+object Main {
+  def main(args: Array[String]) = {
+    //args(0), args(1)
+  }
+}
